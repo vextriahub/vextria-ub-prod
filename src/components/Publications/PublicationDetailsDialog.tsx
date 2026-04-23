c9fd5c687f89f98df0ddbb726988efe02b370522
Gustavo Dantas <gustavodantas.advogados@gmail.com>
2026-04-23 08:10:11 -0300

    fix: syntax error in publication modal v11.1

--- src/components/Publications/PublicationDetailsDialog.tsx
+++ src/components/Publications/PublicationDetailsDialog.tsx
@@ -158,7 +158,6 @@ export const PublicationDetailsDialog = ({ publication, open, onOpenChange, trig
              </Button>
           </div>
         </div>
-    </div>
       </DialogContent>
     </Dialog>
   );
